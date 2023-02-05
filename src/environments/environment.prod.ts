2e865ecf8e8f5ceba65939af0e496cc0cb981efa
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2023-02-05 19:15:47 +0100

    dataGroup added tsome changes in ngsw-config.json and firebase-messaging-sw.js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   url_api: 'https://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyCehjKaeCl1j35pAId6TId-L2YdBQqKvSI',
-    authDomain: 'angular-pwa-platzi.firebaseapp.com',
-    databaseURL: 'https://angular-pwa-platzi.firebaseio.com',
-    projectId: 'angular-pwa-platzi',
-    storageBucket: 'angular-pwa-platzi.appspot.com',
-    messagingSenderId: '1011695246249',
-    appId: '1:1011695246249:web:1f9c4802a5cdff9c8842b8'
-  }
+    apiKey: 'AIzaSyCJtTYxK32k0QBus48vGLs5VpaICbVticY',
+    authDomain: 'curso-angular-pwa.firebaseapp.com',
+    databaseURL: 'https://curso-angular-pwa.web.app',
+    projectId: 'curso-angular-pwa',
+    storageBucket: 'curso-angular-pwa.appspot.com',
+    messagingSenderId: '735057917032',
+    appId: '1:735057917032:web:6e4bdf54a2fd72529537dd'
+}
 };
