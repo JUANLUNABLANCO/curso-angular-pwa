e5b4a6742a5977394ae813e4f72f0c9acb3b8e2b
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2023-02-06 12:57:50 +0100

    update pwa service worker, automatización para las nuevas versiones de la PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { AngularFireMessaging } from '@angular/fire/messaging';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+// import { AngularFireMessaging } from '@angular/fire/messaging';
+// import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 interface Token {
   token: string;
@@ -12,34 +12,32 @@ interface Token {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent
-// implements OnInit
-{
+export class AppComponent implements OnInit {
 
 
   // private tokensCollections: AngularFirestoreCollection<Token>;
 
-  // constructor(
-  //   private swUpdate: SwUpdate,
+  constructor(
+    private swUpdate: SwUpdate,
   //   private messaging: AngularFireMessaging,
   //   private database: AngularFirestore
-  // ) {
+  ) {
   //   this.tokensCollections = this.database.collection<Token>('tokens');
-  // }
+  }
 
-  // ngOnInit() {
-  //   this.updatePWA();
+  ngOnInit() {
+    this.updatePWA();
   //   this.requestPermission();
   //   this.listenNotifications();
-  // }
+  }
 
-  // updatePWA() {
-  //   this.swUpdate.available
-  //   .subscribe(value => {
-  //     console.log('update:', value);
-  //     window.location.reload();
-  //   });
-  // }
+  updatePWA() {
+    this.swUpdate.available
+    .subscribe(value => {
+      console.log('update:', value);
+      window.location.reload();
+    });
+  }
 
   // requestPermission() {
   //   this.messaging.requestToken
